3b24626864b9a8abd8392038b962cc6ccfd01705
Mohammad yusuf iqbal <yusufiqbal150@gmail.com>
2025-03-27 10:33:11 +0530

    header done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,31 +3,49 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
-import { LayoutDashboard } from 'lucide-react'
+import { ChevronDown, LayoutDashboard, StarIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 const Header = () => {
   return (
-    <header >
-      <nav>
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+      <nav className='container mx-auto px-4 h-16 flex items-center justify-between' >
        <Link href="/">
           <Image 
             src={"/logo.png"}
             alt="Yuzsai Logo"
-            height={10}
+            height={200}
             width={1000}
             className="h-12 py-1 w-auto object-contain logoimg"
           />
         </Link>
-        <div>
-          <SignedIn>
-            <Link href={"/dashboard"}>
+
+        <div className=' flex text-center space-x-2 '>
+        <SignedIn>
+            <Link href={""}>
             <Button>
-              <LayoutDashboard className=' ml-8 h-4 w-4'/>
-              Industry dashboard
+            <LayoutDashboard className=' h-4 w-4'/>
+            <span className='hidden md:block'>Industry dashboard</span>
+              
             
             </Button>
             </Link>
           </SignedIn>
+
+          <DropdownMenu>
+  <DropdownMenuTrigger>
+    <Button>
+    <StarIcon className=' h-4 w-4'/>
+    <span className='hidden md:block'>Growth Tools</span>
+    <ChevronDown className=' h-4 w-4 '/>
+    </Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+          
         </div>
         </nav>
 
